9105391bea9fd8587affe91b662ff17c5667521e
Karelly Stephan <172272351+KaStephan@users.noreply.github.com>
2025-11-14 16:19:45 +0100

    Add Conda-Activation-Scripts as submodule

---
+++ activate_environment_windows.bat
@@ -0,0 +1,4 @@
+:: Run conda activation script for Windows
+:: Located in Conda-Activation-Scripts/activate_environment_windows.bat
+
+@call "Conda-Activation-Scripts/activate_environment_windows.bat"
